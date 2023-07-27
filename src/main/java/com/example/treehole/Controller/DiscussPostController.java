87428f1b36b5f4cc6f56522b24a5795b6cfd82e9
Summersover <wangtuoxin@foxmail.com>
2023-07-27 09:27:19 +0800

    帖子详情

--- src/main/java/com/example/treehole/Controller/DiscussPostController.java
+++ src/main/java/com/example/treehole/Controller/DiscussPostController.java
@@ -3,13 +3,13 @@ package com.example.treehole.Controller;
 import com.example.treehole.Entity.DiscussPost;
 import com.example.treehole.Entity.User;
 import com.example.treehole.Service.DiscussPostService;
+import com.example.treehole.Service.UserService;
 import com.example.treehole.Util.HostHolder;
 import com.example.treehole.Util.TreeholeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -22,6 +22,9 @@ public class DiscussPostController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/add")
     @ResponseBody
@@ -40,4 +43,14 @@ public class DiscussPostController {
         // 报错在其他地方统一处理
         return TreeholeUtil.getJsonString(0, "帖子发布成功");
     }
+
+    @GetMapping("/detail/{discussPostId}")
+    public String findDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+        return "/site/discuss-detail";
+    }
+
 }
